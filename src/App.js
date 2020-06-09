5559dc2c4fc946a7edfca8e001070fcc192eb048
Bryan Gregorius <bgregori@redhat.com>
2020-06-09 12:48:51 -0400

    Small change to trigger build via webhook

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn OpenShift
+          Learn OpenShift!
         </a>
       </header>
     </div>
