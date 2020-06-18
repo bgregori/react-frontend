de4a8a754bb5c136ec607b3bdcf5e774ff8caeda
Bryan Gregorius <bgregori@redhat.com>
2020-06-18 15:05:43 -0400

    Add KeyCloak and public/secure components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
+import Welcome from './Welcome';
+import Secured from './Secured';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          API_URL: {window._env_.API_URL}
-        </p>
-        <a
-          className="App-link"
-          href="https://learn.openshift.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn OpenShift!
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="container">
+          <ul>
+            <li><Link to="/">public component</Link></li>
+            <li><Link to="/secured">secured component</Link></li>
+          </ul>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/secured" component={Secured} />
+        </div>
+      </BrowserRouter>
   );
 }
 
